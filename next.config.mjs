0ad284c3b5fd132080dc5c3832354681399797f2
Wael Fezari <wael.fezari@epitech.eu>
2026-09-14 20:19:25 +0100

    Include native PDF parser runtime assets in serverless deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const config = {
   outputFileTracingIncludes: {
-    '/api/rooms/*/documents': ['./node_modules/pdf-parse/dist/**/*', './node_modules/pdfjs-dist/legacy/build/**/*'],
+    '/api/rooms/*/documents': ['./node_modules/pdf-parse/dist/**/*', './node_modules/pdfjs-dist/legacy/build/**/*', './node_modules/@napi-rs/canvas*/**/*'],
     '/api/rooms/*/summary/*': ['./node_modules/pdfkit/js/standard-fonts/**/*', './node_modules/pdfkit/js/data/**/*'],
   },
   serverExternalPackages: [
@@ -9,6 +9,7 @@ const config = {
     "onnxruntime-node",
     "sharp",
     "pdf-parse",
+    "@napi-rs/canvas",
     "@react-pdf/renderer",
   ],
   poweredByHeader: false,
